29a15a5fdcc9a257c1c6ae0187477f3e92234e16
anhthu <anhthu127at@gmail>
2020-11-19 17:43:26 +0700

    release 1.0.0

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Router from './routers/index';
+
+function App() {
+  console.log(11111111);
+  return (
+    <div>
+      <div id="messages"></div>
+      <Router />
+    </div>
+  );
+}
+
+export default App;
